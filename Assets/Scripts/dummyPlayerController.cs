a36a24654bb5de6d2e769244ba567b3285cc387c
Bryan Tai <redhatpieman@gmail.com>
2019-01-26 10:56:56 -0800

    Added WorldController and Warmth logic

--- Assets/Scripts/dummyPlayerController.cs
+++ Assets/Scripts/dummyPlayerController.cs
@@ -8,16 +8,46 @@ public class dummyPlayerController : MonoBehaviour
 
     private Rigidbody rb;
 
+    private float currentWarmth; //If this hits zero, the player becomes too sad to continue :(
+    public float maximumWarmth = 100f;
+    public float minDistanceForWarmth = 2f; //How close the player needs to be to a heat source to gain warmth
+    public float warmthLossPerSecond = 1f;
+    public float warmthGainPerSecond = 20f;
+
+    public WorldController worldController;
+
+    public TextMesh warmthText; //TODO Replace this with another way to display current Warmth. glow effect
+
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+
+        currentWarmth = maximumWarmth;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //Check if Player is near a heat source
+
+        GameObject closestGameObject = worldController.GetClosestHeatSourceToPlayer();
+        float closestDistance = Vector3.Distance(this.transform.position, closestGameObject.transform.position);
+
+        if(closestDistance <= minDistanceForWarmth)
+        {
+            if(currentWarmth < maximumWarmth)
+            {
+                currentWarmth += warmthGainPerSecond * Time.deltaTime;
+            }else
+            {
+                currentWarmth = maximumWarmth;
+            }
+        }else
+        {
+            currentWarmth -= warmthLossPerSecond * Time.deltaTime;
+        }
+        warmthText.text = currentWarmth.ToString("F2");
     }
 
     private void FixedUpdate()
