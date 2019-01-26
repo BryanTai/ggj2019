07d8525a22d10f7490138e2e1bfb1cacafe539d2
RayFlower <xionoblivion@gmail.com>
2019-01-26 01:46:47 -0800

    Child Stuff
    Added a prefab for children, scripted behaviours, and made a basic layout for the game on my test map

---
+++ Assets/Scripts/ChildBehaviour.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[System.Serializable]
+
+public class ChildBehaviour : MonoBehaviour
+{
+    private bool isFollowingPlayer = false;
+    private bool isDroppedOff = false;
+    private float childSize;
+    private Renderer childMaterial;
+
+    public float pickupDistance; // The distance the player needs to be to pick me up
+    public float dropOffDistance; // The distance the player needs to be to drop me off at the bonfire
+    public float followDistance; // the distance at which I will keep when following the player
+    public float followLerpSpeed; // the speed at which I will lerp to the player
+
+    public float childSizeMin;
+    public float childSizeMax;
+    public bool randomizeColour = false; // Whether or not to randomize the children's color
+    public Material[] childMaterialsList;
+    GameObject player; // the player object reference
+    GameObject bonfire;
+
+    private void Awake()
+    {
+        // Find references to objects/components
+        player = GameObject.FindGameObjectWithTag("Player");
+        bonfire = GameObject.FindGameObjectWithTag("Bonfire");
+        childMaterial = GetComponent<Renderer>();
+
+        if (randomizeColour)
+        {
+            // picks a random material to use to display the colour of the child
+            childMaterial.material = childMaterialsList[UnityEngine.Random.Range(0, childMaterialsList.Length)];
+        }
+        else
+        {
+            childMaterial.material = childMaterialsList[0];
+        }
+
+    
+
+        // Picks a random size for the child
+        childSize = UnityEngine.Random.Range(childSizeMin, childSizeMax);
+        transform.localScale = new Vector3(1, 1, 1) * childSize;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // If I have not been picked up yet and the player is within range of me to be picked up
+        if (!isDroppedOff && !isFollowingPlayer && Vector3.Distance(transform.position,player.transform.position) < pickupDistance)
+        {
+            PickedUp();
+        }
+        // if I have been picked up
+        else if (isFollowingPlayer && !isDroppedOff)
+        {
+            // lerp position to player
+            transform.position = Vector3.Lerp(transform.position, player.transform.position, followLerpSpeed);
+        }
+
+        if (isFollowingPlayer && !isDroppedOff && Vector3.Distance(player.transform.position, bonfire.transform.position) < dropOffDistance)
+        {
+            DroppedOff();
+        }
+        else if (isDroppedOff)
+        {
+            transform.position = Vector3.Lerp(transform.position, bonfire.transform.position, followLerpSpeed);
+        }
+    }
+
+    private void DroppedOff()
+    {
+        isFollowingPlayer = false;
+        isDroppedOff = true;
+
+        // Grow bonfire from here
+            // bonfire.childrenDroppedOff++;
+        // play sound
+    }
+
+    private void PickedUp()
+    {
+        isFollowingPlayer = true;
+        // play sound
+    }
+}
---
+++ Assets/Scripts/dummyPlayerController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class dummyPlayerController : MonoBehaviour
+{
+    public float moveSpeed;
+
+    private Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        // Gets the input, forms a vector 3 value and moves the player using the attached rigidbody
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        //Vector3 movement = new Vector3(moveHorizontal, moveVertical, 0.0f);
+
+        rb.velocity = new Vector3(moveHorizontal*moveSpeed,0, moveVertical*moveSpeed);
+    }
+}
