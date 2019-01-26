4f89c3db21e195caf48e2bbfb10ab4ecd6adb34a
RayFlower <xionoblivion@gmail.com>
2019-01-26 10:28:03 -0800

    Made a fancy conga line for followers
    Bunch of code changes adding a better following behaviour for chilren upon the player

--- Assets/Scripts/dummyPlayerController.cs
+++ Assets/Scripts/dummyPlayerController.cs
@@ -1,17 +1,30 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class dummyPlayerController : MonoBehaviour
 {
     public float moveSpeed;
+    public int pathFollowerSteps; // the distance followers will follow the player in a chain
+    public List<Vector3> followerChainPositions;
+    List<Vector3> playerPath = new List<Vector3>(); // List of positions used for player path
+
+    
 
     private Rigidbody rb;
+    private int followerCount = 0;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+
+        // Initializes the follower array
+        for(int i = 0; i < followerChainPositions.Count; i++)
+        {
+            followerChainPositions[i] = new Vector3(0, 0.5f, 0);
+        }
     }
 
     // Update is called once per frame
@@ -28,5 +41,39 @@ public class dummyPlayerController : MonoBehaviour
         //Vector3 movement = new Vector3(moveHorizontal, moveVertical, 0.0f);
 
         rb.velocity = new Vector3(moveHorizontal*moveSpeed,0, moveVertical*moveSpeed);
+
+        // updates the following path only if they are moving
+        if(moveHorizontal != 0 || moveVertical != 0)
+        {
+            // adds position
+            playerPath.Add(transform.position);
+            for(int i = 0; i < followerChainPositions.Count; i++)
+            {
+                // checks if there are enough positions to log a new follower position, exits if there isn't
+                if(playerPath.Count >= pathFollowerSteps * (i + 1))
+                {
+                    followerChainPositions[i] = playerPath[playerPath.Count - pathFollowerSteps*(i+1)];
+                }
+                else
+                {
+                    break;
+                }
+            }
+        }
+    }
+
+    public void IncreaseFollowers()
+    {
+        followerCount++;
+    }
+
+    public int GetFollowers()
+    {
+        return followerCount;
+    }
+
+    public void DecreaseFollowers()
+    {
+        followerCount--;
     }
 }
