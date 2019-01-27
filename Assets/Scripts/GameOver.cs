5b2e5aff541c96d41a351c651ac251334fbf7473
margaret <m_yao11@hotmail.com>
2019-01-27 11:31:26 -0800

    game over scene once the player runs out of warmth

---
+++ Assets/Scripts/GameOver.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameOver : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public dummyPlayerController player;
+    public float restartDelay = 5f;
+
+    private Animator anim;
+    private float restartTimer;
+    
+    void Awake()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (player.currentWarmth <= 0)
+        {
+            anim.SetTrigger("GameOver");
+
+            restartTimer += Time.deltaTime;
+
+            if (restartTimer >= restartDelay)
+            {
+                SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+            }
+        }
+    }
+}
