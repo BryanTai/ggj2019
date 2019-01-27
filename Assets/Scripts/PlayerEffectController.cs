90423715323e26bb5925f94d56dfca43ecc2558c
maskedchamo <stuff@brunobaron.com>
2019-01-26 18:58:51 -0800

    added player effect controller script added new bonfire effect removed torch effect

--- Assets/Scripts/PlayerEffectController.cs
+++ Assets/Scripts/PlayerEffectController.cs
@@ -7,5 +7,27 @@ public class PlayerEffectController : MonoBehaviour
     public ParticleSystem fireEffect;
     public ParticleSystem fireEmbers;
 
+    //set warmth level between 0 and 1
+    // 0 = cold, no fire effect
+    // 1 = warm, full fire effect
+    public float warmthLevel = 1;
 
+    
+
+    private void Update()
+    {
+        //fireEffect modulation
+        var fireEffectEmission = fireEffect.emission;
+        fireEffectEmission.rateOverTimeMultiplier = warmthLevel * 20.0f;
+
+        var fireEffectVelocity = fireEffect.velocityOverLifetime;
+        fireEffectVelocity.radialMultiplier = Mathf.Lerp(0.2f, 0.5f, warmthLevel);
+
+        //embers modulation
+        var fireEmbersEmission = fireEmbers.emission;
+        fireEmbersEmission.rateOverTimeMultiplier = warmthLevel * 20.0f;
+
+        var fireEmbersSpeed = fireEmbers.velocityOverLifetime;
+        fireEmbersSpeed.speedModifierMultiplier = Mathf.Lerp(0.5f, 1.0f, warmthLevel);
+    }
 }
